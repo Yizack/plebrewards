dc11378e7f81201d44a48df7ad2241128dd0b998
Yizack Rangel <yizackr@gmail.com>
2024-05-01 17:21:12 -0500

    chore: refresh token on each request

--- server/api/webhooks/twitch.post.ts
+++ server/api/webhooks/twitch.post.ts
@@ -30,6 +30,23 @@ export default defineEventHandler(async (event) => {
 
     await spotifyAPI.refreshToken(connection?.refresh_token);
     const isURL = Spotify.isValidTrackURL(webhook.user_input);
+    const user = await DB.select({
+      refresh_token: tables.users.refresh_token
+    }).from(tables.users).where(eq(tables.users.id_user, Number(webhook.broadcaster_user_id))).get();
+
+    const response = await twitchAPI.refreshToken(user?.refresh_token);
+
+    if (!response) {
+      throw createError({
+        statusCode: ErrorCode.BAD_REQUEST,
+        message: "Failed to get access token"
+      });
+    }
+
+    if (response.refresh_token !== user?.refresh_token) {
+      await DB.update(tables.users).set({ refresh_token: response.refresh_token }).where(eq(tables.users.id_user, Number(webhook.broadcaster_user_id))).run();
+    }
+
     if (!isURL) {
       const search = await spotifyAPI.searchTrack({
         q: webhook.user_input,
@@ -43,7 +60,7 @@ export default defineEventHandler(async (event) => {
         await spotifyAPI.addToQueue(search.tracks.items[0].id).catch(() => null);
         const trackName = search.tracks.items[0].name;
         const trackArtists = search.tracks.items[0].artists.map((artist) => artist.name).join(", ");
-        await twitchAPI.sendChatMessage(webhook.broadcaster_user_id, `Added ${trackArtists} - ${trackName} to the queue.`).catch(() => null);
+        await twitchAPI.sendChatMessage(webhook.broadcaster_user_id, `Added ${trackArtists} - ${trackName} to the queue.`)
       }
     }
     else {
@@ -53,7 +70,7 @@ export default defineEventHandler(async (event) => {
       if (track) {
         const trackName = track.name;
         const trackArtists = track.artists.map((artist) => artist.name).join(", ");
-        await twitchAPI.sendChatMessage(webhook.broadcaster_user_id, `Added ${trackArtists} - ${trackName} to the queue.`).catch(() => null);
+        await twitchAPI.sendChatMessage(webhook.broadcaster_user_id, `Added "${trackArtists} - ${trackName}" to the queue.`).catch(() => null);
       }
     }
   }
