2c7b6a591af20d67101230e37e0d937149aca47f
Yizack Rangel <yizackr@gmail.com>
2024-05-01 16:25:47 -0500

    fix: delete link response

--- server/api/spotify/link.delete.ts
+++ server/api/spotify/link.delete.ts
@@ -12,8 +12,16 @@ export default defineEventHandler(async (event) => {
   }
 
   const DB = useDB();
-  return DB.delete(tables.connections).where(and(eq(tables.connections.id_user, body.id_user), eq(tables.connections.type, "spotify"))).returning({
-    id_user: tables.connections.id_user,
-    type: tables.connections.type
-  }).get();
+  const response = await DB.delete(tables.connections).where(and(eq(tables.connections.id_user, body.id_user), eq(tables.connections.type, "spotify"))).returning().get();
+  if (!response) {
+    throw createError({
+      statusCode: ErrorCode.NOT_FOUND,
+      message: "Connection not found."
+    });
+  }
+
+  return {
+    id_user: response.id_user,
+    type: response.type
+  };
 });
