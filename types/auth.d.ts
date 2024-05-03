334fb208b435000c9160652c5bd641e6d14a18b0
Yizack Rangel <yizackr@gmail.com>
2024-05-03 12:55:47 -0500

    chore: string type

--- types/auth.d.ts
+++ types/auth.d.ts
@@ -16,7 +16,7 @@ declare module "#auth-utils" {
       access_token: string;
       expires_in: number;
       refresh_token: string;
-      token_type: "bearer"
+      token_type: string;
     }
   }
 
