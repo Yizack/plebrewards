196ec653122250d33735e0f3ce130ff34e551a00
Yizack Rangel <yizackr@gmail.com>
2024-05-05 01:41:37 -0500

    chore: login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,29 @@
 <script setup lang="ts">
-const { loggedIn, user, clear } = useUserSession();
+const { loggedIn } = useUserSession();
+
+const login = async () => {
+  if (loggedIn.value) return navigateTo("/app");
+  return navigateTo("/api/auth/twitch", { external: true });
+};
 </script>
 
 <template>
   <main>
-    <div v-if="loggedIn">
-      <h1>Welcome {{ user?.login }}!</h1>
-      <div class="mb-2">
-        <NuxtLink to="/app">Go to the APP</NuxtLink>
+    <div class="container d-flex justify-content-center align-items-center vh-100">
+      <div class="col-lg-8 mx-auto" style="height: 80%">
+        <div class="d-flex flex-column flex-lg-row border rounded-3 bg-body-secondary overflow-hidden h-100">
+          <div class="border-end col-lg-6" style="background-image: url('/images/login.jpg'); background-size: cover; min-height: 30%;">
+            <h1 class="m-0 p-4">Pleb Rewards</h1>
+          </div>
+          <div class="col-lg-6 p-5">
+            <h2 class="mb-4">Sign In</h2>
+            <button class="btn btn-lg btn-twitch w-100 mb-2 d-flex align-items-center justify-content-center gap-2" @click="login">
+              <Icon name="bi:twitch" size="1.3rem" />
+              <span>Sign in with Twitch</span>
+            </button>
+          </div>
+        </div>
       </div>
-      <button class="btn btn-primary rounded-pill" @click="clear">Logout</button>
-    </div>
-    <div v-else>
-      <h1>Not logged in</h1>
-      <a href="/api/auth/twitch">Login with Twitch</a>
     </div>
   </main>
 </template>
