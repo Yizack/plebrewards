618cfef334aba8d91dd176ed04229791db48ea14
Yizack Rangel <yizackr@gmail.com>
2024-05-06 17:20:43 -0500

    chore: add tittle

--- .config/nuxt.ts
+++ .config/nuxt.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
-      title: "",
+      title: SITE.name,
       htmlAttrs: {
         lang: "en"
       },
